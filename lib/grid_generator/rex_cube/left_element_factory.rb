c2e69675a09cffae450c59b135fac85359bf0dee
Mark Humphreys <mark@mrlhumphreys.com>
2023-06-04 14:23:38 +1000

    fix rex cube

--- lib/grid_generator/rex_cube/left_element_factory.rb
+++ lib/grid_generator/rex_cube/left_element_factory.rb
@@ -10,9 +10,9 @@ module GridGenerator
           bottom_left_corner: Matrix.column_vector([0, 3*units]),
           bottom_right_corner: Matrix.column_vector([3*units, 4.5*units]),
 
-          center_top: Matrix.column_vector([1.75*units, 2.25*units]),
+          center_top: Matrix.column_vector([1.5*units, 1.75*units]),
           center_right: Matrix.column_vector([2*units, 2.5*units]),
-          center_bottom: Matrix.column_vector([2.75*units, 3.25*units]),
+          center_bottom: Matrix.column_vector([1.5*units, 2.75*units]),
           center_left: Matrix.column_vector([1*units, 2*units])
         }
       end
